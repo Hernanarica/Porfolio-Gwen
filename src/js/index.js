1bbcf49ea68384686272014a775e9cc804c4197b
Hernan Arica <hernanaricammm@gmail.com>
2021-04-30 15:42:54 -0300

    Header listo y responsibe

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+const $menuBtn  = document.querySelector('.header__button');
+const $iconMenu = document.querySelector('.header__button img');
+const $menu     = document.querySelector('.header__menu');
+
+$menuBtn.addEventListener('click', e => {
+	if (!e.target.classList.toggle('close')) {
+		changeStatusMenu('src/imgs/icon__menu-open.svg', 'flex', 'translateX(0)', true, 50);
+	} else {
+		changeStatusMenu('src/imgs/icon__menu-close.svg', 'none', 'translateX(-100%)', false, 350);
+	}
+});
+
+/**
+ * función que aplica la animación
+ *
+ * @param {string} path
+ * @param {string} display
+ * @param {string} fx
+ * @param {boolean} timeStatus
+ * @param {number} time
+ */
+function changeStatusMenu(path, display, fx, timeStatus, time) {
+	$iconMenu.src = path;
+
+	if (timeStatus) {
+		$menu.style.display = display;
+		setTimeout(() => {
+			$menu.style.transform = fx;
+		}, time);
+	} else {
+		$menu.style.transform = fx;
+		setTimeout(() => {
+			$menu.style.display = display;
+		}, time);
+	}
+}
