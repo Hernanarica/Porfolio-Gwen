9cfa15365fa60985c79b7b916b3c6a17c4269342
Hernán <hernanaricammm@gmail.com>
2021-05-10 19:37:57 -0300

    Navbar actualizado v2

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,21 @@
-const $menuBtn  = document.querySelector('.header__button');
-const $iconMenu = document.querySelector('.header__button img');
-const $menu     = document.querySelector('.header__menu');
-const $links    = document.querySelectorAll('.header__link');
+const $menuBtn    = document.querySelector('.header__button');
+const $iconMenu   = document.querySelector('.header__button img');
+const $menu       = document.querySelector('.header__menu');
+const queryStatus = window.matchMedia("(max-width: 768px)");
 
-$links.forEach($link => {
-	$link.addEventListener('click', () => {
-		if (!$menuBtn.classList.toggle('close')) {
-			changeStatusMenu('src/assets/icons/icon__menu-open.svg', 'flex', 'translateX(0)', true, 50);
-		} else {
-			changeStatusMenu('src/assets/icons/icon__menu-close.svg', 'none', 'translateX(-100%)', false, 350);
-		}
+if (queryStatus.matches) {
+	const $links = document.querySelectorAll('.header__link');
+
+	$links.forEach($link => {
+		$link.addEventListener('click', () => {
+			if (!$menuBtn.classList.toggle('close')) {
+				changeStatusMenu('src/assets/icons/icon__menu-open.svg', 'flex', 'translateX(0)', true, 50);
+			} else {
+				changeStatusMenu('src/assets/icons/icon__menu-close.svg', 'none', 'translateX(-100%)', false, 350);
+			}
+		});
 	});
-});
+}
 
 $menuBtn.addEventListener('click', e => {
 	if (!e.target.classList.toggle('close')) {
