001853d9c6419479eb79a5cbe8eea4c6d1f1b4fe
Hernán <hernanaricammm@gmail.com>
2021-05-10 15:24:44 -0300

    Navbar arreglado

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
 const $menuBtn  = document.querySelector('.header__button');
 const $iconMenu = document.querySelector('.header__button img');
 const $menu     = document.querySelector('.header__menu');
+const $links    = document.querySelectorAll('.header__link');
+
+$links.forEach($link => {
+	$link.addEventListener('click', () => {
+		if (!$menuBtn.classList.toggle('close')) {
+			changeStatusMenu('src/assets/icons/icon__menu-open.svg', 'flex', 'translateX(0)', true, 50);
+		} else {
+			changeStatusMenu('src/assets/icons/icon__menu-close.svg', 'none', 'translateX(-100%)', false, 350);
+		}
+	});
+});
 
 $menuBtn.addEventListener('click', e => {
 	if (!e.target.classList.toggle('close')) {
